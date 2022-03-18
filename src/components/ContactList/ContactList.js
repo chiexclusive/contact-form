6fda3a385943fb956c47d6df732921e1549cb8b5
mykelmatics <mykelmatics@gmail.com>
2022-03-18 02:58:33 +0100

    finished contact implementation

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,12 +1,47 @@
+/** @format */
+
 // ============================CONTACT FORM COMPONENT==============================
 // ================================================================================
 
-import "./ContactList.css"
+import "./ContactList.css";
 
-function ContactList () {
-	return (
-		<div>Contact List works</div>
-	);
+function ContactList({contacts}) {
+
+  return (
+    <div className="contact-list col-xs-12 col-sm-12 col-md-12 col-lg-6 row justify-content-end">
+      <div className="col-xs-12 col-sm-12 col-md-11 card p-3">
+        <header className="w-100 d-flex justify-content-center align-items-center flex-column">
+          <h5>Contact list</h5>
+        </header>
+        <section className="w-100">
+          <table className="w-100 table table-responsive table-borderless">
+            <thead>
+              <tr>
+                <td>Email</td>
+                <td>Country</td>
+                <td>State</td>
+                <td>City</td>
+              </tr>
+            </thead>
+			<tbody>
+				{
+					JSON.parse(contacts).map((contact, contactIndex) => {
+						return (
+							<tr key={ contactIndex}>
+								<td>{contact.email}</td>
+								<td>{contact.country}</td>
+								<td>{contact.state}</td>
+								<td>{contact.city || '-'}</td>
+							</tr>
+            			);
+					})
+				}
+			</tbody>
+          </table>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default ContactList;
--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,20 +1,39 @@
+/** @format */
+
 // ========================== CONTACT PAGE =============================
 // =====================================================================
-import './ContactPage.css';
+import "./ContactPage.css";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import ContactList from "../../components/ContactList/ContactList";
 import BackgroundContents from "../../components/BackgroundContents/BackgroundContents";
+import { useState, useEffect } from "react";
 
 function ContactPage() {
-	return (
-		<div className = "w-100 h-100">
-			<BackgroundContents />
-		  	<div className = "d-flex row container w-100 h-100">
-		  		<ContactForm/>
-		  		<ContactList/>
-		  	</div>
-	  	</div>
-	);
+	const [contacts, setContacts] = useState(localStorage.getItem("xyz-contacts") || '[]');
+
+  // USE EFFECT HOOK TO POPULATE CONTACT LIST FROM LOCAL STORAGE
+  useEffect(() => {
+	  localStorage.setItem("xyz-contacts", contacts);
+  }, [contacts]);
+
+  //   CALL BACK
+  const updateContact = (contact) => {
+	  let theContacts = JSON.parse(contacts);
+	  theContacts.unshift(contact)
+	  setContacts(JSON.stringify(theContacts))
+  }
+
+  return (
+    <div className="w-100 h-100">
+      <BackgroundContents />
+      <div className="w-100 main container d-flex justify-content-center">
+        <div className="w-100 row justify-content-center">
+          <ContactForm updateContact={updateContact} />
+          <ContactList contacts={contacts} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ContactPage;
